3de9d451d2951b114762f06732305f0091d5622a
Jake <jakeroggenbuck2@gmail.com>
2023-04-07 16:07:40 -0700

    Add more + usage

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-char *last_dir(char *path) {
+void usage() { printf("usage: readme [options]\n"); }
+
+char *last_dir(char *path, bool verbose) {
     int i = 0;
     int last_slash = 0;
     char *last_path_buf;
     int last_path_len;
-    int verbose = 0;
 
     while (path[i]) {
         if (path[i] == '/') {
@@ -38,14 +40,18 @@ char *last_dir(char *path) {
     return last_path_buf;
 }
 
-void create_readme(char *fname) {
+void create_readme(char *fname, bool verbose) {
     char cwd[PATH_MAX];
 
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        char *dirname = last_dir(cwd);
+        char *dirname = last_dir(cwd, verbose);
+        if (verbose) {
+            printf("Trying to create README.md for %s...\n", dirname);
+        }
 
         FILE *f;
         f = fopen("README.md", "w");
+        printf("Created README.md for %s!\n", dirname);
 
         if (f == NULL) {
             printf("Unable to create file.\n");
@@ -62,11 +68,12 @@ void create_readme(char *fname) {
 
 int main() {
     char *fname = "README.md";
+    bool verbose = false;
 
     if (access(fname, F_OK) == 0) {
         printf("%s already exists.\n", fname);
     } else {
-        create_readme(fname);
+        create_readme(fname, verbose);
     }
 
     return 0;
